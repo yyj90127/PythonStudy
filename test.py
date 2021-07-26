c10e5db568381aea81afac6a9c4b2ffa8daa6407
YYJ-macbook <yyj90127@qq.com>
2021-07-26 11:07:29 +0800

    update unittest2

--- test.py
+++ test.py
@@ -1,10 +1,4 @@
-import time
 import datetime
 
-t = time.time()
-
-print (int(round(t * 1000)))    #毫秒级时间戳
-
-now_time = datetime.datetime.now()
-A = str(datetime.datetime.now()).replace('-','').replace(':','').replace(' ','')[:-7]
-print(A)
+a = datetime.date.today() - datetime.timedelta(days=1)
+print(a)
